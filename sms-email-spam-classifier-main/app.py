5f0904572b4bbfa6f5c74e0612593668f81b53ca
Chaitali-Gaikwad <106470893+chaitaliig@users.noreply.github.com>
2024-02-29 00:05:42 +0530

    Update app.py

--- sms-email-spam-classifier-main/app.py
+++ sms-email-spam-classifier-main/app.py
@@ -33,7 +33,7 @@ def transform_text(text):
     return " ".join(y)
 
 tfidf = pickle.load(open('sms-email-spam-classifier-main/vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('sms-email-spam-classifier-main/model.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
